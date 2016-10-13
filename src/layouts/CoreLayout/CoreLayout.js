63bdeb365b41ba5ea3e7756cfb4c76364ba1e8e7
Jacob Hash <jhash@umich.edu>
2016-10-13 01:26:51 -0700

    Combined CoreLayout divs, added footer, added various social links without links working

--- src/layouts/CoreLayout/CoreLayout.js
+++ src/layouts/CoreLayout/CoreLayout.js
@@ -9,6 +9,13 @@ export const CoreLayout = ({ children }) => (
     <div className='container text-center core-layout__viewport'>
       {children}
     </div>
+    <div className='footer'>
+      <a className='footer__link--social' href='facebook.com' target='_blank'><i className='footer__link--social__icon mdi mdi-facebook' /></a>
+      <a className='footer__link--social' href='twitter.com' target='_blank'><i className='footer__link--social__icon mdi mdi-twitter' /></a>
+      <a className='footer__link--social' href='linkedin.com' target='_blank'><i className='footer__link--social__icon mdi mdi-linkedin' /></a>
+      <a className='footer__link--social' href='stackoverflow.com' target='_blank'><i className='footer__link--social__icon mdi mdi-stackoverflow' /></a>
+      <a className='footer__link--social' href='github.com' target='_blank'><i className='footer__link--social__icon mdi mdi-github-circle' /></a>
+    </div>
   </div>
 )
 
